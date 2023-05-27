cb9a894370ad2e0e7c70d8766ffb6f30aa0f34e2
MylanvV-666 <12200295@student.pxl.be>
2023-05-27 17:03:08 +0200

    home klaar zonder responsiviteit

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.getElementById("lets_go_button").addEventListener("click", doorgaan);
 
 function doorgaan() {
     let next = document.getElementById("naam").value;
-    localStorage.setItem("naam", next);
+    document.getElementById("naamhome").innerHTML = next;
 }
-let naam2 = localStorage.getItem("naam");
-document.getElementById("naamhome").innerHTML = naam2;
+doorgaan();
