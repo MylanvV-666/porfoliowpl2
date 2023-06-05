495ef3aaf48401418755de394e22427a9b319a79
MylanvV-666 <12200295@student.pxl.be>
2023-06-05 23:34:57 +0200

    enkel responsiviteit nog

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let input = "";
-if (window.location.filename === 'index.html') {
-    input = document.getElementById("naam").value;
-    localStorage.setItem("myInputValue", input);
-} else if (window.location.filename === 'home.html') {
-    input = localStorage.getItem("myInputValue");
-    document.getElementById("naamhome").innerHTML = input;
+window.onload = doorgaan;
+document.getElementById("lets_go_button").addEventListener("click", doorgaan);
+function doorgaan(){
+    let input = "";
+    if (window.location.pathname === '/portfolio/index.html') {
+        input = document.getElementById("naam").value;
+        console.log("Saving input value:", input);
+        localStorage.setItem("myInputValue", input);
+    }
+    if (window.location.pathname === '/portfolio/home.html') {
+        input = localStorage.getItem("myInputValue");
+        console.log("Retrieved input value:", input);
+        document.getElementById("naamhome").innerHTML = input;
+    }
 }
+doorgaan();
+
