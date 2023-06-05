66ea4be40668fd11cf49db77a7d373bc9c1a025f
MylanvV-666 <12200295@student.pxl.be>
2023-06-05 20:04:17 +0200

    bijna klaar enkel java nog en de responsiviteit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//ask page
-document.getElementById("lets_go_button").addEventListener("click", doorgaan);
-
-function doorgaan() {
-    let next = document.getElementById("naam").value;
-    document.getElementById("naamhome").innerHTML = next;
+let input = "";
+if (window.location.filename === 'index.html') {
+    input = document.getElementById("naam").value;
+    localStorage.setItem("myInputValue", input);
+} else if (window.location.filename === 'home.html') {
+    input = localStorage.getItem("myInputValue");
+    document.getElementById("naamhome").innerHTML = input;
 }
-doorgaan();
