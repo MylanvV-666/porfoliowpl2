4b9994f08f3d4abe8856a3e089491c7806449fe2
MylanvV-666 <113908325+MylanvV-666@users.noreply.github.com>
2023-08-25 13:31:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ window.onload = doorgaan;
 document.getElementById("lets_go_button").addEventListener("click", doorgaan);
 function doorgaan(){
     let input = "";
-    if (window.location.pathname === '/portfolio/index.html' || window.location.href === 'https://portfolio-mylan-van-vugt.netlify.app/') {
+    if (window.location.pathname === '/index.html' || window.location.href === 'https://mylan-van-vugt.netlify.app') {
         input = document.getElementById("naam").value;
         console.log("Saving input value:", input);
         localStorage.setItem("myInputValue", input);
     }
-    if (window.location.pathname === '/portfolio/home.html' || window.location.href === 'https://portfolio-mylan-van-vugt.netlify.app/home.html') {
+    if (window.location.pathname === '/home.html' || window.location.href === 'https://mylan-van-vugt.netlify.app/home.html') {
         input = localStorage.getItem("myInputValue");
         console.log("Retrieved input value:", input);
         document.getElementById("naamhome").innerHTML = input;
